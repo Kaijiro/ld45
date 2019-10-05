7fdbfde5217cdbe5e26eaa14d7e2000645049f55
Tristan Lasalle <tristan.lasalle@gmail.com>
2019-10-05 15:51:26 +0200

    Add a script making the camera follow the player

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+
+    public Transform targetToFollow;
+    public float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetToFollow != null)
+        {
+            Vector3 cameraPosition = transform.position;
+            Vector3 targetPosition = targetToFollow.position;
+            Vector3 targetPositionVector = new Vector3(targetPosition.x, targetPosition.y, cameraPosition.z);
+
+            Vector3 velocity = (targetPositionVector - cameraPosition) * moveSpeed;
+            transform.position = Vector3.SmoothDamp(cameraPosition, targetPositionVector, ref velocity, 1.0f, Time.deltaTime);
+        }
+    }
+}
