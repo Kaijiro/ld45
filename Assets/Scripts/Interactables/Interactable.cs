506b5a3391194175db8d5aa211454d2ae6d7ba92
Tristan Lasalle <tristan.lasalle@gmail.com>
2019-10-05 18:01:52 +0200

    Add an interaction system

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 1f;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(255, 0, 0, 0.3f);
+        Gizmos.DrawSphere(transform.position, radius);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public virtual void OnInteract()
+    {
+        Debug.Log("Interaction of " + name + " is triggered");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInteractionController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionController : MonoBehaviour
+{
+    private Interactable _closestInteractable;
+    private float _closestInteractableDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _closestInteractable = null;
+        _closestInteractableDistance = 10000f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 playerPosition = transform.position;
+
+            Interactable[] interactables = FindObjectsOfType<Interactable>();
+            foreach (Interactable interactable in interactables)
+            {
+                float interactableDistance =
+                    Vector3.Distance(interactable.GetComponent<Transform>().position, playerPosition);
+                
+                if (_closestInteractable == null)
+                {
+                    setInteractableAsClosest(interactable, interactableDistance);
+                    continue;
+                }
+                
+                if (interactableDistance < _closestInteractableDistance)
+                {
+                    _closestInteractable = interactable;
+                    _closestInteractableDistance = interactableDistance;
+                }
+            }
+
+            if (_closestInteractableDistance < _closestInteractable.radius)
+            {
+                _closestInteractable.OnInteract();
+            }
+        }
+    }
+
+    private Interactable setInteractableAsClosest(Interactable interactable, float interactableDistance)
+    {
+        _closestInteractable = interactable;
+        _closestInteractableDistance = interactableDistance;
+    }
+}
